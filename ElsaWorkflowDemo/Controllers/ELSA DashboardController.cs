d3428adb3af574d637d4073388634c75a516ce2e
Narmadaw <narmadaw9@gmail.com>
2024-02-19 09:01:53 -0800

    add controller/view

---
+++ ElsaWorkflowDemo/Controllers/ELSA DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElsaWorkflowDemo.Controllers
+{
+    public class ELSA_DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
